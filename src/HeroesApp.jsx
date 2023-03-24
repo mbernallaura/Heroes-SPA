8bb13d88789a2943801725c3e4cd0d2d419b89a5
mbernallaura <mbernallaura@gmail.com>
2023-03-24 18:28:42 -0500

    Instalando Route

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,10 @@
+import { AppRouter } from "./router/AppRouter"
 
 
 export const HeroesApp = () => {
     return (
         <>
-            <h1>Heroes App</h1>  
+            <AppRouter/>
         </>
     )
 }
