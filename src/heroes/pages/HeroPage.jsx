dc6929d836e3de1a9081a2993a5204dc16af7d96
mbernallaura <mbernallaura@gmail.com>
2023-04-03 17:49:14 -0500

    Direccionar la pagina del heroe que el usuario clickeo y obtener los datos de ese heroe

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,14 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroByid } from "../helpers";
 
 
 export const HeroPage = () => {
+    const { id } = useParams();
+    const hero = getHeroByid( id );
+    if(!hero){
+        return <Navigate to={"/marvel"}/>
+    }
+
     return (
         <h1>HeroPage</h1>
     )
