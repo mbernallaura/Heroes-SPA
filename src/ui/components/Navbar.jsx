e7c9c6945b9ba98396deb3905940542051d569c7
mbernallaura <mbernallaura@gmail.com>
2023-05-04 19:01:16 -0500

    Por medio dio del useContext se llamara el user para saber que nombre de usuario entro y que se visualice en el navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
+    const { user } = useContext( AuthContext );
     const navigate = useNavigate();
 
     const onLogout = () =>{
@@ -39,7 +42,10 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-info'>Laura</span>
+                    <span className='nav-item nav-link text-info'>
+                        {/* el signo de interrogacion pregunta si es null que no continue */}
+                        { user?.name }
+                        </span>
                     <button 
                         className='nav-item nav-link btn'
                         onClick={ onLogout }
