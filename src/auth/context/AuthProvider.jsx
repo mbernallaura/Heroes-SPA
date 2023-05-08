e6dc5cba7f2614a35bcef15742ea787048b1364c
mbernallaura <mbernallaura@gmail.com>
2023-05-08 18:10:24 -0500

    Remover el usuario que ingreso del localstorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -18,20 +18,28 @@ const init = () =>{
 export const AuthProvider = ({ children }) => {
 
     const [ authState, dispatch ] = useReducer( authReducer, {}, init );
+
     const login = (name = '') =>{
         const user = { id: '1', name }
-        const action = {
-            type: types.login,
-            payload: user
-        };
+        const action = { type: types.login, payload: user };
         localStorage.setItem('user', JSON.stringify( user ));
         dispatch( action );
     }
 
+    const logout = () =>{
+        localStorage.removeItem('user');
+        const action = { type: types.logout };
+        dispatch(action);
+    }
+    
+
     return (
         <AuthContext.Provider value={{ 
             ...authState,
-            login: login
+
+            //methods
+            login: login,
+            logout
         }}>
             { children }
         </AuthContext.Provider>
